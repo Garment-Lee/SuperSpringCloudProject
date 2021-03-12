f7d71f8feff8f988a89e43677f8247d421d69163
garmentlee <garment1991@163.com>
2021-03-12 19:51:03 +0800

    update:1.add gateway module.

---
+++ gf-gateway/src/main/java/com/ligf/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.ligf.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName GatewayApplication
+ * @Description 网关服务启动类
+ * @Author Garment Lee
+ * @Date 2021/3/12 11:21
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
